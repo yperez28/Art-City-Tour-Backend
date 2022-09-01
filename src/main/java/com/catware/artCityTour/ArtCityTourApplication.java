d8f2ca433c4b17a67abfec207230ecbd400248e5
Crystel Montero <crysvane05@estudiantec.cr>
2022-09-01 15:56:54 -0600

    spring boot app

---
+++ src/main/java/com/catware/artCityTour/ArtCityTourApplication.java
@@ -0,0 +1,13 @@
+package com.catware.artCityTour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtCityTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtCityTourApplication.class, args);
+	}
+
+}
