00eb6346dd7aa87d7d246d213d4f53a332dcbc60
Crystel Montero <crysvane05@estudiantec.cr>
2022-10-08 19:41:51 -0600

    login image

--- src/main/java/com/catware/artCityTour/Controller/ImagesController.java
+++ src/main/java/com/catware/artCityTour/Controller/ImagesController.java
@@ -1,14 +1,24 @@
 package com.catware.artCityTour.Controller;
 
 import com.catware.artCityTour.Constants.Constants;
+import com.catware.artCityTour.Service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
+@RestController()
+@RequestMapping(value = "/images")
 public class ImagesController {
 
-    public String getLinkToSaveInDB(String googleDriveShareLink){
-        String[] imgLinkSplit = googleDriveShareLink.split("/");
-        String id = imgLinkSplit[imgLinkSplit.length-2];
-        return Constants.VIEW_LINK + id;
+    @Autowired
+    private ImageService imageService;
+
+    @CrossOrigin
+    @RequestMapping(value = "/getLogin", method = RequestMethod.GET)
+    public String loginImagePath(){
+        return imageService.getImageLogin();
     }
 }
