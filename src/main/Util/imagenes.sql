65d521a02c6d6158e3e7e40759f1f0b1b22ee460
Crystel Montero <crysvane05@estudiantec.cr>
2022-12-16 21:14:36 -0600

    imagenes

---
+++ src/main/Util/imagenes.sql
@@ -0,0 +1,58 @@
+/*
+COMO UTILIZAR ESTE ARCHIVO:
+
+Para completar el segundo parametro de cada "VALUES" debe ir a google drive -> seleccionar la imagen que usted guardó
+y quiere que sea la mostrada en cada sección correspondiente -> click dercho -> compartir u obtener link -> copiar 
+el link que le da (no tiene que especificar ningún permiso de lectura/escritura) 
+que se ve así: "https://drive.google.com/file/d/1RFplm_jG74zrIDniXw2Yy02x9XFQBAoG/view?usp=share_link" 
+y tomar la parte númerica después de "file/d/" lo que sigue (1RFplm_jG74zrIDniXw2Yy02x9XFQBAoG) se considera el ID de 
+la imágen, esto se coloca después de "id" en cada link de cada INSERT de abajo y luego corre todo este archivo
+
+
+*/
+
+INSERT INTO public.image(
+	name, drive_path)
+	VALUES ('Login', 'https://drive.google.com/uc?id=');
+	
+INSERT INTO public.image(
+	name, drive_path)
+	VALUES ('Lugares', 'https://drive.google.com/uc?id=');
+	
+INSERT INTO public.image(
+	name, drive_path)
+	VALUES ('Patrocinadores', 'https://drive.google.com/uc?id=');
+
+INSERT INTO public.image(
+	name, drive_path)
+	VALUES ('Noticias', 'https://drive.google.com/uc?id=');
+
+INSERT INTO public.image(
+	name, drive_path)
+	VALUES ('Noticias', 'https://drive.google.com/uc?id=');
+
+INSERT INTO public.image(
+	name, drive_path)
+	VALUES ('Usuarios', 'https://drive.google.com/uc?id=');
+
+INSERT INTO public.image(
+	name, drive_path)
+	VALUES ('Ediciones', 'https://drive.google.com/uc?id=');
+
+INSERT INTO public.image(
+	name, drive_path)
+	VALUES ('Eventos', 'https://drive.google.com/uc?id=');
+
+INSERT INTO public.image(
+	name, drive_path)
+	VALUES ('Rutas', 'https://drive.google.com/uc?id=');
+
+INSERT INTO public.image(
+	name, drive_path)
+	VALUES ('Principal Page', 'https://drive.google.com/uc?id=');
+
+INSERT INTO public.image(
+	name, drive_path)
+	VALUES ('Membership', 'https://drive.google.com/uc?id=');
+	
+
